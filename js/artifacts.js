12e23ac49adc876daf5fb2f16931947dbfd7725f
jpdhmc <112502289+jpdhmc@users.noreply.github.com>
2023-12-19 22:11:46 -0600

    better breaking up of functions

--- js/artifacts.js
+++ js/artifacts.js
@@ -5,16 +5,8 @@
  */
 const init = () => {
     console.log("artifacts.js init running");
-    // document.getElementById("exampleButton").addEventListener("click", indexButtonClick);
-    const posters = document.getElementsByClassName("posterWrapper");
-    // cant use foreach because its an htmlcollection not an array
-    for (let i = 0; i < posters.length; i++) {
-        let posterImg = posters[i].querySelector("img");
-        posters[i].addEventListener('mouseover', () => {posterImg.style.transform = "scale(1.05,1.05)"});
-        posters[i].addEventListener('mouseout', () => {posterImg.style.transform = "scale(1,1)"});
-        posters[i].addEventListener('mousemove', (e) => {posterImg.style.transformOrigin = ((e.pageX - posters[i].offsetLeft) / posters[i].offsetWidth) * 100 + "% "
-                                                                                            + ((e.pageY - posters[i].offsetTop) / posters[i].offsetHeight) * 100 + "%"});
-    }
+    document.getElementById("exampleButton").addEventListener("click", indexButtonClick);
+    handlePosterImages();
 }
 
 const indexButtonClick = () => {
@@ -23,6 +15,20 @@ const indexButtonClick = () => {
     const posterVideoOne = document.getElementById("posterVideoOne");
     indexPoster.style.display = "none";
     posterVideoOne.style.display = "block";
+}
+
+// Poster image background zooming and panning
+const handlePosterImages = () => {
+    const posters = document.getElementsByClassName("posterWrapper");
+    // Can't use foreach because it's an htmlcollection not an array
+    for (let i = 0; i < posters.length; i++) {
+        let posterImg = posters[i].querySelector("img");
+        posters[i].addEventListener('mouseover', () => {posterImg.style.transform = "scale(1.05,1.05)"});
+        posters[i].addEventListener('mouseout', () => {posterImg.style.transform = "scale(1,1)"});
+        // Uses mousemove event to calculate horizontal and vertical position of the mouse relative to the posterWrapper element
+        posters[i].addEventListener('mousemove', (e) => {posterImg.style.transformOrigin = ((e.pageX - posters[i].offsetLeft) / posters[i].offsetWidth) * 100 + "% "
+                                                                                            + ((e.pageY - posters[i].offsetTop) / posters[i].offsetHeight) * 100 + "%"});
+    }
 }
 
 window.onload = init;
