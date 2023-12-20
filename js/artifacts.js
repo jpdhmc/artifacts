0028eaa7d69b6b03d2ca64d57ff0e3ecb85f90ce
jpdhmc <112502289+jpdhmc@users.noreply.github.com>
2023-12-19 20:13:01 -0600

    poster background zoom working, pan in progress

--- js/artifacts.js
+++ js/artifacts.js
@@ -5,7 +5,15 @@
  */
 const init = () => {
     console.log("artifacts.js init running");
-    document.getElementById("exampleButton").addEventListener("click", indexButtonClick);
+    // document.getElementById("exampleButton").addEventListener("click", indexButtonClick);
+    const posters = document.getElementsByClassName("posterWrapper");
+    // cant use foreach because its an htmlcollection not an array
+    for (let i = 0; i < posters.length; i++) {
+        let posterImg = posters[i].querySelector("img");
+        posters[i].addEventListener('mouseover', () => {posterImg.style.transform = "scale(1.1,1.1)"});
+        posters[i].addEventListener('mouseout', () => {posterImg.style.transform = "scale(1,1)"});
+        posters[i].addEventListener('mousemove', (e) => {posterImg.style.transform-origin = ((e.pageX - posters[i].offsetLeft) / posters[i].offsetWidth) * 100})
+    }
 }
 
 const indexButtonClick = () => {
