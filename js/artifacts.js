b5f4acca75f9887603a04c60a3a314d8e484bb7c
jpdhmc <112502289+jpdhmc@users.noreply.github.com>
2023-12-30 14:10:24 -0600

    Reworked how filenames are requested to now use promises

--- js/artifacts.js
+++ js/artifacts.js
@@ -10,38 +10,38 @@ const init = () => {
 }
 
 const indexButtonClick = () => {
-    console.log("example button clicked");
     const indexPoster = document.getElementById("indexPoster");
     const posterVideoOne = document.getElementById("posterVideoOne");
     //indexPoster.style.display = "none";
     document.getElementById("exampleButton").style.display = "none";
     //posterVideoOne.style.display = "block";
-    requestFilenames();
+
+    requestFilenames().then(fileNames => {
+        showGallery(fileNames);
+    }).catch(error => {
+        console.error("Error: ", error);
+    });
 }
 
 // Use an xmlhttprequest to get a string containing the filenames from the server
 const requestFilenames = () => {
-    let file = "include/asdf.txt"
+    return new Promise((resolve, reject) => {
+        let file = "include/asdf.txt"
 
-    let xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-            if (this.status == 200) {
-                handleFilenames(this.responseText);
-            } else if (this.status == 404) {
-                handleFilenames("error.png");
-                console.log("Document containing filenames was not found on server");
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4) {
+                if (this.status == 200) {
+                    let fileNames = this.responseText.split(" ");
+                    resolve(fileNames);
+                } else {
+                    reject(new Error("Request for file names failed"));
+                }
             }
         }
-    }
-    xhttp.open("GET", file, true);
-    xhttp.send()
-}
-
-// Split the filenames string into an array
-const handleFilenames = (fileText) => {
-    let fileNames = fileText.split(" ");
-    console.log(fileNames);
+        xhttp.open("GET", file, true);
+        xhttp.send()
+    });
 }
 
 // Build and display the gallery
@@ -63,18 +63,18 @@ const handlePosterMovers = () => {
 
     for (let i = 0; i < posters.length; i++) {
         let posterMover = posters[i].querySelector(".posterMover");
-        posters[i].addEventListener('mouseover', () => {posterMover.style.transform = "scale(1.05,1.05)"});
-        posters[i].addEventListener('mouseout', () => {posterMover.style.transform = "scale(1,1)"});
+        posters[i].addEventListener('mouseover', () => { posterMover.style.transform = "scale(1.05,1.05)" });
+        posters[i].addEventListener('mouseout', () => { posterMover.style.transform = "scale(1,1)" });
 
         // Uses mousemove event to calculate horizontal and vertical position of the mouse relative to the posterWrapper element
-        posters[i].addEventListener('mousemove', (e) => {posterMover.style.transformOrigin = calculateTransformOrigin(e, posters[i])})
+        posters[i].addEventListener('mousemove', (e) => { posterMover.style.transformOrigin = calculateTransformOrigin(e, posters[i]) })
     }
 }
 
 // Calculate transform of elements based on relative mouse position, to be called on mousemove
 const calculateTransformOrigin = (ev, element) => {
     let trOrigin = ((ev.pageX - element.offsetLeft) / element.offsetWidth) * 100 + "% "
-    + ((ev.pageY - element.offsetTop) / element.offsetHeight) * 100 + "%"
+        + ((ev.pageY - element.offsetTop) / element.offsetHeight) * 100 + "%"
     return trOrigin;
 }
 
